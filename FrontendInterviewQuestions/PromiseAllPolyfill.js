bac277d29e85d53b947249975b06c6ba2c6d63ae
Sahil Aggarwal <sahil2.aggarwal@paytm.com>
2020-10-11 18:44:11 +0530

    Added polyfill of promise.All asked during major companies frontend developer interview

---
+++ FrontendInterviewQuestions/PromiseAllPolyfill.js
@@ -0,0 +1,37 @@
+/* Promise.all Polyfill
+   One of the interview questions asked for mid to senior web
+   developr roles.
+*/
+
+const PromiseAllPolyfill = function (arr) {
+    return new Promise(function (resolve, reject) {
+      if (!arr || typeof arr.length === 'undefined') throw new TypeError('Promise.all accepts an array');
+      var args = Array.prototype.slice.call(arr);
+      if (args.length === 0) return resolve([]);
+      var remaining = args.length;
+
+      function res(i, val) {
+        try {
+          if (val && (typeof val === 'object' || typeof val === 'function')) {
+            var then = val.then;
+            if (typeof then === 'function') {
+              then.call(val, function (val) {
+                res(i, val);
+              }, reject);
+              return;
+            }
+          }
+          args[i] = val;
+          if (--remaining === 0) {
+            resolve(args);
+          }
+        } catch (ex) {
+          reject(ex);
+        }
+      }
+
+      for (var i = 0; i < args.length; i++) {
+        res(i, args[i]);
+      }
+    });
+  };
